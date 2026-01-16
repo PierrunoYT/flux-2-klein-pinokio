64d3ee92421a31b92d7f6cb0fe2cd06ac3d4b85d
PierrunoYT <pierrebruno@hotmail.ch>
2026-01-16 13:23:13 +0100

    Remove unused shell command for upgrading Python dependencies in update.js

--- update.js
+++ update.js
@@ -7,15 +7,6 @@ module.exports = {
       }
     },
     {
-      method: "shell.run",
-      params: {
-        venv: "env",
-        message: [
-          "uv pip install --upgrade -r requirements.txt"
-        ]
-      }
-    },
-    {
       method: "notify",
       params: {
         html: "Update complete! The launcher and dependencies have been updated to the latest versions."
